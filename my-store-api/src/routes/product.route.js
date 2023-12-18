1f08dc23bc8244b92e5419a0672d9a4517a2ecb6
Edouard <edouard.lukau91@gmail.com>
2023-12-19 00:54:51 +0100

    Création de la route de l'api pour le filtre

--- my-store-api/src/routes/product.route.js
+++ my-store-api/src/routes/product.route.js
@@ -5,5 +5,6 @@ const productController = require('../controllers/product.controller');
 
 router.get('/:id', productController.getProduct);
 router.get('/', productController.getProducts);
+router.get('/filters', productController.getProducts);
 
 module.exports = router;
--- my-store-front/src/services/api/product.api.js
+++ my-store-front/src/services/api/product.api.js
@@ -26,3 +26,24 @@ export async function getProduct(id) {
         return err;
     }
 }
+
+
+export async function getProductsAjax(intervalMin, intervalMax ) {
+
+    try {
+
+        const res = await fetch(`${process.env.BACKEND_URL}/api/products/filters?min=${intervalMin}&max=${intervalMax}`, {
+            cache: "no-store",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await res.json();
+         console.log(data)
+        return data;
+    }
+    catch (err) {
+        console.log('error : ' + `${process.env.BACKEND_URL}/api/products/filers?=&min=${intervalMin}&max=${intervalMax}` )
+        return err;
+    }
+}
