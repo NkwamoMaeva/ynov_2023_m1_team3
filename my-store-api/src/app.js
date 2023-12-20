4d5cc4673ee177002e660622e856c4563624ff7a
Amidou <amidou.diallo@perfectogroupe.fr>
2023-12-20 22:20:42 +0100

    envoi de mail reussi

--- my-store-api/src/app.js
+++ my-store-api/src/app.js
@@ -3,8 +3,22 @@ const cors = require('cors');
 const errorHandler = require('./middlewares/errorsHandling');
 const config = require('./config');
 const routes = require('./routes');
+const mail = require('nodemailer');
 
 const app = express();
+
+// Configuration de Nodemailer
+const transporter = mail.createTransport({
+    host: 'smtp.gmail.com',  // Serveur SMTP de Gmail
+    port: 587,
+    secure: false,  
+    auth: {
+        user: 'mystoreynov@gmail.com',  
+        pass: 'bkdjfkdfzuagoadl', 
+    },
+    logger: true,
+    debug: true,
+});
 
 // parse json request body
 app.use(express.json());
@@ -16,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
     cors(
         {
-            origin: config.frontend_url,
+            origin: "*",
         },
     ),
 );
@@ -27,6 +41,34 @@ app.use(express.static(__dirname + '/public'));
 // initial route
 app.get('/', (req, res) => {
     res.send({ message: 'Welcome to app-store-api application.' });
+});
+
+// Nouvelle route pour l'envoi d'e-mail
+app.post('/api/send-email', async (req, res) => {
+    try {
+        console.log(req.body);
+
+        const { to, subject, text } = req.body;
+
+        if (!to || !subject || !text) {
+            return res.status(400).send('Les champs to, subject et text sont requis.');
+        }
+
+        const mailOptions = {
+            from: 'mystoreynov@gmail.com', 
+            to: to,
+            subject: subject,
+            text: text,
+        };
+
+        const result = await transporter.sendMail(mailOptions);
+        console.log('E-mail envoyé: %s', result.messageId);
+
+        res.status(200).send('E-mail envoyé avec succès!');
+    } catch (error) {
+        console.log('Erreur lors de l\'envoi de l\'e-mail', error);
+        res.status(500).send('Erreur lors de l\'envoi de l\'e-mail');
+    }
 });
 
 // api routes prefix
