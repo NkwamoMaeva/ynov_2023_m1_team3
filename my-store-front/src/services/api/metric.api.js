c8115cf694da0edc6806d0e61e136a7ba23de2f1
Edouard <edouard.lukau91@gmail.com>
2023-12-21 02:03:17 +0100

    feat(Metrics) moyenne min et max

--- my-store-front/src/services/api/metric.api.js
+++ my-store-front/src/services/api/metric.api.js
@@ -1,3 +1,34 @@
+function generateAverages (response){ //géneration des moyennes à partir de la reponse de la reqûete get
+
+    let data = response.data;
+    // console.log(data)
+
+    let averages  = { // objet qui stockera les valeurs finales
+        min : null,
+        max : null
+    }
+    let minValuesArray = [] //toutes les valeurs minimum
+    let maxValuesArray = [] //toutes les valeurs maximum
+
+    data.forEach(elem => {
+
+        minValuesArray.push(elem.min)
+    });
+    // console.log(minValuesArray)
+
+    data.forEach(elem => {
+        maxValuesArray.push(elem.max)
+    });
+
+    // console.log(maxValuesArray)
+
+    averages.min =  Math.ceil((minValuesArray.reduce((acc,currentVal)=>{ return  parseInt(acc) + parseInt(currentVal)}) / minValuesArray.length ))
+    averages.max =  Math.ceil((maxValuesArray.reduce((acc,currentVal)=>{ return  parseInt(acc) + parseInt(currentVal)}) / maxValuesArray.length ))
+
+    console.log(averages);
+    return averages;
+}
+
 
 export async function getFilterMetrics() {
     try {
@@ -8,7 +39,7 @@ export async function getFilterMetrics() {
             },
         });
         const data = await res.json();
-        console.log(data)
+        generateAverages(data) // cette fonction retoune un object qui contiendra les moyennes
         return data;
     }
     catch (err) {
@@ -34,4 +65,5 @@ export async function postFilterValues(min, max) {
         return err;
     }
 }
+
 
